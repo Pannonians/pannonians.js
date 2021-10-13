a978d4254ed4193a07e2d788c31495c756784fa3
ajgorp <igor.popovic@pannonians.com>
2021-10-13 11:08:12 +0200

    Dodao button

--- apps/todo/src/App.js
+++ apps/todo/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 
 function App() {
+  const sayHello = () => {
+    console.log("Hello");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Pannonians.js hello world!</h1>
+        <button onClick = {sayHello}>Hello</button>
       </header>
     </div>
   );
