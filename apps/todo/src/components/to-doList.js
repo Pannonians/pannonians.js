3852262fe7e583f7553d6d754421a0d29d397daf
dragicamladenovic <dragica.mladenovic@pannonians.com>
2021-10-13 13:20:49 +0200

    export todo function

---
+++ apps/todo/src/components/to-doList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const todo = () =>{
+    return(
+        <div className="todo-containar">
+            <ul className="todo-list"></ul>
+        </div>
+    )
+}
+
+export default todo;
