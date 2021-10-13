b047b5f57603941ffd2bbe8f45900d086e572f03
dragicamladenovic <dragica.mladenovic@pannonians.com>
2021-10-13 12:12:31 +0200

    add + icon

--- apps/todo/src/components/Form.js
+++ apps/todo/src/components/Form.js
@@ -4,12 +4,12 @@ import React from "react";
 const Form = () =>{
     return(
         <form>
-            <input type="text" className name name="App-form"></input>
-            <button className name="App-form-button" type="submit">
-                <i className name="App-fas fa-plus-square"></i>
+            <input type="text" className name="App-form"></input>
+            <button className ="App-form-button" type="submit">
+                <i className ="fas fa-plus-square"></i>
             </button>
-            <div className name="select">
-                <select names="todos" className name="filter-todo">
+            <div className ="select">
+                <select names="todos" className ="filter-todo">
                     <option value="all">All</option> 
                     <option value="done">Done</option>
                     <option value="not done">Not done</option>
