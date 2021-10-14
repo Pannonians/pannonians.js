0b614351f91b02893fcb76cc46a583b6d39fdf37
dragicamladenovic <dragica.mladenovic@pannonians.com>
2021-10-14 15:49:54 +0200

    some change

--- apps/todo/src/components/ToDoList.js
+++ apps/todo/src/components/ToDoList.js
@@ -1,13 +1,24 @@
 import React from "react";
 //import components
-import Todo from './Todo';
+import Todo from "./Todo";
 
-const ToDoList = () =>{
-    return(
-        <div className="todo-containar">
-            <ul className="todo-list"></ul>
-            <ToDoList/>
-        </div>
-    );
-}
+const ToDoList = ({ todos, setTodos }) => {
+  console.log(todos);
+  return (
+    <div className="todo-containar">
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            todo={todo}
+            text={todo.text}
+            key={todo.id}
+          />
+        ))}
+        {/* <Todo/> */}
+      </ul>
+    </div>
+  );
+};
 export default ToDoList;
