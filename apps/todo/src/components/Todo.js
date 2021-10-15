002db1a24d113f4af78d2437c34b31b12875b263
dragicamladenovic <dragica.mladenovic@pannonians.com>
2021-10-15 08:54:34 +0200

    new const completeHandler

--- apps/todo/src/components/Todo.js
+++ apps/todo/src/components/Todo.js
@@ -1,14 +1,28 @@
 import React from "react";
 
 const Todo = ({ text, todo, todos, setTodos }) => {
-    //Events
-    const deleteHandler = () =>{
+  //Events
+  const deleteHandler = () => {
     setTodos(todos.filter((element) => element.id !== todo.id));
-    };
+  };
+
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
   return (
     <div className="todo">
-      <li className="todo-item">{text}</li>
-      <button className="complete-button">
+      <li className= {`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+      <button onClick={completeHandler} className="complete-button">
         <i className="fas fa-check"></i>
       </button>
       <button onClick={deleteHandler} className="trash-button">
