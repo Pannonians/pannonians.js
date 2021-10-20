a774ceb1b1c6b96f0fa8eb4a1832e2d1d73e1601
pvukmanovic-pannonians <petar.vukmanovic@pannonians.com>
2021-10-20 14:39:10 +0200

    added axios and made first posts request

--- apps/blog/src/App.js
+++ apps/blog/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+const POSTS_URL = `${BASE_URL}/posts`;
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    const { data } = await axios.get(POSTS_URL);
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{JSON.stringify(posts)}</div>
       </header>
     </div>
   );
