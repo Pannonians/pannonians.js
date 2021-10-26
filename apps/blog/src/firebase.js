e9c4f5ea8d0b489d303cd38632478050e71a8326
pvukmanovic-pannonians <90039609+pvukmanovic-pannonians@users.noreply.github.com>
2021-10-26 12:03:42 +0200

    20211026.2 Auth and Routing (#4)
    * Added auth
    
    Auth and pages and router for our app
    
    * Fixed build errors

---
+++ apps/blog/src/firebase.js
@@ -0,0 +1,48 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDzsc_f6ILwMoAZmhBd0gIgm3_r8XkR-2o",
+  authDomain: "pannonians-blog.firebaseapp.com",
+  projectId: "pannonians-blog",
+  storageBucket: "pannonians-blog.appspot.com",
+  messagingSenderId: "1001060730143",
+  appId: "1:1001060730143:web:4e727f0d995272ce969c2a",
+  measurementId: "G-8E7QNMP4TR",
+};
+
+const Firebase = (function () {
+  let instance;
+
+  function createInstance() {
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    const auth = getAuth(app);
+    const db = getFirestore(app);
+    return {
+      app,
+      analytics,
+      auth,
+      db,
+      signInWithPopup,
+    };
+  }
+
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    },
+  };
+})();
+
+export default Firebase;
