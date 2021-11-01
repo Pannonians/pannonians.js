0fae84c1d501696535f50e6edfbe15516a484ae8
TijanaGolovic <tijana.golovic@pannonians.com>
2021-11-01 12:27:41 +0100

    Promenjen naziv fajla Login.js

--- apps/blog/src/Login/Login.js
+++ apps/blog/src/Login/Login.js
@@ -23,7 +23,7 @@ export default function Signup() {
   if (isAuthenticated === null) return null;
   if (isAuthenticated === true) return <Redirect to="/dashboard" />;
   return (
-    <div clasName = "mainContainer">
+    <div clasName="mainContainer">
       <button onClick={(event) => signup(event, GoogleAuthProvider)}>
         Login with Google
       </button>
