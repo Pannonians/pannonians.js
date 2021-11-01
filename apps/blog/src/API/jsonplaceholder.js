6441549b0f840a3291cc62bd8a31b5af77fc2aa2
TijanaGolovic <tijana.golovic@pannonians.com>
2021-11-01 11:46:44 +0100

    Dodate CRUD operacije

--- apps/blog/src/API/jsonplaceholder.js
+++ apps/blog/src/API/jsonplaceholder.js
@@ -11,9 +11,26 @@ import axios from "axios";
 const BASE_URL = "https://jsonplaceholder.typicode.com";
 const POSTS_URL = `${BASE_URL}/posts`;
 
-const axios = require("axios").default;
-
 export const getPosts = async () => {
   const { data } = await axios.get(POSTS_URL);
   return data;
+};
+
+export const postShow = async (id) => {
+  const { data } = await axios.get(`${POSTS_URL}/${id}`);
+  return data;
+};
+
+export const postCreate = async (requestBody) => {
+  const { data } = await axios.post(POSTS_URL, requestBody);
+  return data;
+};
+
+export const postUpdate = async (id, requestBody) => {
+  const { data } = await axios.patch(`${POSTS_URL}/${id}`, requestBody);
+  return data;
+};
+
+export const deletePost = async (id) => {
+  await axios.delete(`${POSTS_URL}/${id}`);
 };
