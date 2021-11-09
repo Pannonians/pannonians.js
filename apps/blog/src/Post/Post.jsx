bdf0a7433f30b97e7f59f2f1b459adb1e1b539d9
dragicamladenovic <dragica.mladenovic@pannonians.com>
2021-11-09 22:35:22 +0100

    creating component Post

---
+++ apps/blog/src/Post/Post.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './style.css';
+import Card from '../UI/Card/Card';
+import BlogPost from '../components/BlogPost/BlogPost';
+import Sidebar from '../components/Sidebar/Sidebar';
+
+
+
+/**
+* @author
+* @function Post
+**/
+
+
+const Post = (props) => {
+ 
+  console.log(props)
+
+//   const mapPost = posts.data.map(postsId => postsId.id)
+// console.log(mapPost);
+  
+  return(
+    <section className="container margin">
+      <BlogPost {...props} />
+      <Sidebar {...props}/>
+    </section>
+   );
+  
+    }
+
+  export default Post;
