513972927775ca48fe530cdeafe94e78fcd4bd28
dragicamladenovic <dragica.mladenovic@pannonians.com>
2021-11-09 22:26:21 +0100

    creating component Home

---
+++ apps/blog/src/containers/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./style.css"
+
+/**
+* @author
+* @function Home
+**/
+
+const Home = (props) => {
+  return(
+    <div className="home">Home</div>
+   )
+  }
+
+  export default Home;
