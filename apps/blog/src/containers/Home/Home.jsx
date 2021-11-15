226a067f5d344bb49a416daa331eb776475bb238
dragicamladenovic <dragica.mladenovic@pannoninas.com>
2021-11-15 09:17:07 +0100

    adding title, text and button "Read Moder" on Home page

--- apps/blog/src/containers/Home/Home.jsx
+++ apps/blog/src/containers/Home/Home.jsx
@@ -57,6 +57,13 @@ const Home = (props) => {
           <div className="postImageWrapper">
           <img src={picture4} alt=""/>
           </div>
+
+          <div style={{textAlign: 'center'}}>
+            <h2>Sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h2>
+            <span>Quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas
+               totam\nnostrum rerum est autem sunt rem eveniet architecto</span><br></br>
+               <button>Read More</button>
+          </div>
           
         </Card>
 
