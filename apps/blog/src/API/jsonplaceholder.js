92a1be72ae630bd2f3484549c541177a68f1c3cc
dragicamladenovic <dragica.mladenovic@pannonians.com>
2021-11-17 23:45:16 +0100

    adding some comments

--- apps/blog/src/API/jsonplaceholder.js
+++ apps/blog/src/API/jsonplaceholder.js
@@ -7,9 +7,14 @@
  */
 
 import axios from "axios";
+// import jsonPostsFile from "../posts.json"
 
 const BASE_URL = "https://jsonplaceholder.typicode.com";
 const POSTS_URL = `${BASE_URL}/posts`;
+
+// const jsonFile = jsonPostsFile;
+// console.log(jsonPostsFile)
+
 
 export const getPosts = async () => {
   const { data } = await axios.get(POSTS_URL);
@@ -25,6 +30,12 @@ export const postCreate = async (requestBody) => {
   const { data } = await axios.post(POSTS_URL, requestBody);
   return data;
 };
+
+// export const postCreateFile = async (requestBody) => {
+//   const { data } = await axios.post(jsonFile, requestBody);
+//   return data;
+// };
+
 
 export const postUpdate = async (id, requestBody) => {
   const { data } = await axios.patch(`${POSTS_URL}/${id}`, requestBody);
