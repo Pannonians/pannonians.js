900e39e6aad3e7c21696b47c1e8d232a3d6af35b
Dragica Mladenovic <dragica.mladenovic@pannonians.com>
2021-12-09 22:53:39 +0100

    Revert "Revert "logEvent for posts""
    This reverts commit 098de37d7e5ea7332e395fa66665f4434a0e9028.

--- apps/blog/src/components/PostForm/PostForm.jsx
+++ apps/blog/src/components/PostForm/PostForm.jsx
@@ -7,8 +7,6 @@ import { useHistory } from "react-router-dom";
 import "react-quill/dist/quill.snow.css";
 import ReactQuill from "react-quill";
 import { logEvent } from '@firebase/analytics'
-import analytics from '../../firebase.js'
-
 
 
 
@@ -30,20 +28,24 @@ const PostForm = (props) => {
 
         const instance = Firebase.getInstance();
         const database = instance.db;
+        const analytics = instance.analytics;
+        console.log(analytics)
         const docRef = await addDoc(collection(database, "posts"), {
             title: title,
             post: post,
             createdAt: serverTimestamp()
         })
+        
+        console.log()
+        setTitle("");
+        setPost("");
+        alert("Post is submitted successfully")
+
         logEvent(analytics, 'create_post', {
             title: title,
             // post: post,
            
           })
-        console.log()
-        setTitle("");
-        setPost("");
-        alert("Post is submitted successfully")
         history.push("/allPosts")
         
 
