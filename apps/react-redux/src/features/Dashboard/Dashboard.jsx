9b7831ba4ffd038744e4ef7712409661637fbe5d
ajgorp <igor.popovic@pannonians.com>
2022-01-17 12:30:31 +0100

    Sredjen Dashboard

--- apps/react-redux/src/features/Dashboard/Dashboard.jsx
+++ apps/react-redux/src/features/Dashboard/Dashboard.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import  picMovies  from "../../img/picMovies.jpg"
+import  picTv  from "../../img/picTV.jpg"
 
 
 export default function Dashboard() {
   return (
     <div>
-      <NavLink to="/movies">Movies</NavLink>
-      <NavLink to="/tvshows">TV Shows</NavLink>
+    <div className="navPics">
+    
+    
+   
+      <NavLink to="/movies"><img style={{width: "80%", borderRadius: "5%"}} src= {picMovies}></img></NavLink>
+      
+      <NavLink to="/tvshows"><img style={{width: "80%", borderRadius: "5%"}} src={picTv}></img></NavLink>
+    </div>
+    <div className="sectionTags"><h1>MOVIES</h1><h1>TV SHOWS</h1></div>
     </div>
   );
 }
