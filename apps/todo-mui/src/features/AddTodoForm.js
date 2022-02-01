4d4d568c33792a4f6f78cb80926d94aa3ceab036
Igor Popovic <igor.popovic@pannonians.com>
2022-02-01 16:05:19 +0100

    gotovo

--- apps/todo-mui/src/features/AddTodoForm.js
+++ apps/todo-mui/src/features/AddTodoForm.js
@@ -1,37 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { addTodo } from "../redux/todoSlice"
-import Button from "@mui/material/Button"
-import TextField from '@mui/material/TextField';
+import { addTodo } from "../redux/todoSlice";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+
 
 const AddTodoForm = () => {
-	const [value, setValue] = useState('');
-	const dispatch = useDispatch()
+  const [value, setValue] = useState("");
+  const dispatch = useDispatch();
 
-	const onSubmit = (event) => {
-		event.preventDefault();
-		console.log('user entered: ' + value);
-		dispatch(addTodo({
-			title: value,
-		}))
-	};
+  const submitTodo = (event) => {
+    event.preventDefault();
+    // console.log(`submit ${value}`);
+    dispatch(
+      addTodo({
+        title: value,
+        completed: false,
+        id: Date.now(),
+      })
+    );
+	setValue("");
+  };
 
-	return (
-		<form onSubmit={onSubmit} className='form-inline mt-3 mb-3'>
-			
-			<TextField
-				type='text'
-				
-				placeholder='Add todo...'
-				value={value}
-				onChange={(event) => setValue(event.target.value)}
-			></TextField>
+  return (
+    <form onSubmit={submitTodo} style={{width:"100%", display: "flex", flexDirection:"row", justifyContent:"space-between"}}>
+      <TextField style={{width:"600px"}}
+        type="text"
+        placeholder="Add todo..."
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      ></TextField>
 
-			<Button type='submit' >
-				Submit
-			</Button>
-		</form>
-	);
+      <Button style={{width:"200px", border:"solid", borderWidth:"2px"}} size="medium" type="submit">Submit</Button>
+    </form>
+  );
 };
 
 export default AddTodoForm;
