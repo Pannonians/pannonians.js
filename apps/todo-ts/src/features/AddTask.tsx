7b312dea72e76d8f661ecddecb8b91d5c78554f3
TijanaGolovic <tijana.golovic@pannonians.com>
2022-02-09 14:22:39 +0100

    added alert and changed id value

--- apps/todo-ts/src/features/AddTask.tsx
+++ apps/todo-ts/src/features/AddTask.tsx
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
 import Button from '@mui/material/Button';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addTodo } from "./todoSlice";
 import TextField from '@mui/material/TextField';
+import { selectTodos } from "../store";
+
 
 
 const AddTask = () => {
   const [value, setValue] = useState("");
+  const todos = useSelector(selectTodos);
 
   const dispatch = useDispatch();
 
   const onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
+    if (!value.length) {
+      return alert("Please add task");
+    }
     dispatch(addTodo({
 		text: value,
     completed: false,
-    id: Date.now()
+    id: todos.length +1
 	}))
 	setValue("");
+  
+  
   };
 
   return (
